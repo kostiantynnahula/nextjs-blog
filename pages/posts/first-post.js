e49530a5088027f6bcb9123fb5eaee6574cec4f2
Kostia Nahula <kostyannagula@gmail.com>
2023-07-16 12:46:03 +0300

    updated layout component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,14 @@
-import Link from 'next/link';
+import Head from 'next/head';
+import Layout from './../../components/layout';
 
 const FirstPost = () => {
-  return <>
-    <h1>First post</h1>
-    <h2>
-      <Link href="/">Back to home</Link>
-    </h2>
-  </>
+  return (
+    <Layout>
+      <Head>
+        <title>First post</title>
+      </Head>
+    </Layout>
+  )
 }
 
 export default FirstPost;
